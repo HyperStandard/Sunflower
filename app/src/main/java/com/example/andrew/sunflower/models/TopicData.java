de66b7472f3a8c7c5c897f96061e313150cb590f
Andrew Lee <nonexistentcharacter@gmail.com>
2015-12-11 16:58:56 -0600

    small commit

---
+++ app/src/main/java/com/example/andrew/sunflower/models/TopicData.java
@@ -0,0 +1,16 @@
+package com.example.andrew.sunflower.models;
+
+/**
+ * Created by Andrew on 12/11/2015.
+ */
+public class TopicData
+{
+	public final String Title;
+	public final String User;
+
+	TopicData( String Title, String User )
+	{
+		this.Title = Title;
+		this.User = User;
+	}
+}
